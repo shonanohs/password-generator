34ba2edab00771c1f5847bc1277f7cb8744de2fc
Shona Fitzpatrick <71610718+shonanohs@users.noreply.github.com>
2025-07-16 16:46:02 +0100

    Added 'generate password' functionality & styling

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-import './style.css'
+import './index.css'
 
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordLength = 15;
+
+function generatePassword() {
+    let password = "";
+    for (let i = 0; i < passwordLength; i++) {
+        const randIdx = Math.floor(Math.random() * characters.length);
+        password += characters[randIdx];
+    }
+    displayPassword(password);
+}
+
+function displayPassword(password) {
+    let passwordElement = document.getElementById("password");
+    passwordElement.innerText = password;
+    console.log(password);
+}
+
+window.generatePassword = generatePassword;
